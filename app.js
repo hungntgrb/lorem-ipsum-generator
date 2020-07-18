1cc10b4efe8876522e3bef70effdbd08720f5d15
Hung Nguyen <hungnt89@gmail.com>
2020-07-18 22:36:21 +0700

    html

---
+++ app.js
@@ -0,0 +1,12 @@
+ps = [
+  "anxiety resolution profession phone aspect cookie assistant coffee virus comparison data environment idea mood cell death ear president sample arrival insurance tea imagination moment wood permission reputation conclusion area property",
+  "facade talk red motorcycle infect proclaim switch battle chew cutting navy wear tactic crossing inject medieval tension minister offend tire preparation conflict include vigorous transport morale conceive parallel map statement",
+  "include hate dorm thread press recovery exercise hair miserable deliver occasion inspiration lend fashionable layer leave behave intention kit transfer wriggle peasant mystery staircase restaurant output agent key shelf round",
+  "writer toast pipe youth guide disgrace tempt electronics subway lid apparatus chain cheese appreciate uncertainty grandmother confession excess investigation escape dominate smash charge expansion waterfall shareholder rush ankle implicit hospital",
+  "mass appearance bomb charge deter accent reputation sale offend question respectable chart barrel disgrace commission date comfortable start pull shiver economy extend knit treat kit hen nature item arm prospect",
+  "speech bed overlook loud feminist false hero decay chart basin copper give racism ally lend seize sport release resident aloof nest wheat official issue application technique lineage consciousness fossil dawn orientation text",
+  "fixture preparation nervous budge practical deck infinite comedy wreck punish stock jelly fog calendar bishop rice pollution care happen slant inquiry treat nationalism report urgency wild perfect shell career chalk iron fast",
+  "owl standard predict echo crude omission reward tempt folklore shock pound terms heroin advantage husband culture stop rape unique ensure pot double exaggerate unity flawed groan or volume coffee challenge retirement humor",
+  "half Venus contrast wound ignore experience sight tactic host behave brainstorm butterfly catch virtue camp compact weed manual teacher forward nightmare soap detector remember length laser dose work discount cook man jump",
+  "sensitive peanut boot distribute product rib farewell regulation lock agreement shaft graduate copper expose bread organ lift carry bargain trainer beach basic sofa rhetoric cemetery jealous crosswalk favourite context debut coffin criminal",
+];
